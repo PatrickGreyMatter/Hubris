0820bc4746f1849dc17f30e96d6ac0da8bc6b29f
Patrick Mennechez <dev.patrick.legre@gmail.com>
2024-05-19 10:39:58 +0200

    commit changes whole project

---
+++ database/migrations/2024_05_16_213042_create_media_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('type');
+            $table->string('director');
+            $table->string('lenght');
+            $table->string('year');
+            $table->string('thumbnail', 255);
+            $table->string('video_url', 255);
+            $table->decimal('average_rating', 2, 1)->default(0);
+            $table->integer('favorite_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
+    }
+};
