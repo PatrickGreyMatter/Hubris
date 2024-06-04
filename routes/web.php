bb8c1235e82d8115d432dcc4794f40eb76995940
PatrickGreyMatter <dev.patrick.legre@gmail.com>
2024-06-04 18:33:20 +0200

    ajout du footer du site

--- routes/web.php
+++ routes/web.php
@@ -45,13 +45,12 @@ Route::get('/email/verify', function () {
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
- 
+
     return redirect('/profil');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+})->middleware(['auth'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
- 
+
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
