08c9d513e6dc06f5f15b868889cf97f66b894550
PatrickGreyMatter <dev.patrick.legre@gmail.com>
2024-06-04 21:36:57 +0200

    ajout du carousel

--- resources/views/partials/_carousel.blade.php
+++ resources/views/partials/_carousel.blade.php
@@ -25,7 +25,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 1</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                       </div>
                     </div>
@@ -34,7 +34,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 2</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -44,7 +44,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 3</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -54,7 +54,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 4</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -72,7 +72,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 5</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                       </div>
                     </div>
@@ -81,7 +81,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 6</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -91,7 +91,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 7</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -101,7 +101,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 8</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -118,7 +118,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 9</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                       </div>
                     </div>
@@ -127,7 +127,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 10</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -137,7 +137,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 11</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
@@ -147,7 +147,7 @@
                     <div class="card">
                       <img class="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
                       <div class="card-body">
-                        <h4 class="card-title">Special title treatment</h4>
+                        <h4 class="card-title">card 12</h4>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
   
                       </div>
